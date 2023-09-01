1a5ff41d4477917b1a923e7da8d43c1f8efd91c3
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-09-01 14:06:12 +0200

    Add firebase example code

--- src/api.js
+++ src/api.js
@@ -1,3 +1,58 @@
+// Firebase setup and use code added only as an example
+// import { initializeApp } from "firebase/app";
+// import {
+//   getFirestore,
+//   collection,
+//   doc,
+//   getDocs,
+//   getDoc,
+//   query,
+//   where
+// } from "firebase/firestore/lite"
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD_k3v3HK3tKEqhlqFHPkwogW7PqEqhGhk",
+//   authDomain: "vanlife-a1af5.firebaseapp.com",
+//   projectId: "vanlife-a1af5",
+//   storageBucket: "vanlife-a1af5.appspot.com",
+//   messagingSenderId: "803007000356",
+//   appId: "1:803007000356:web:446cd3a1ca406839258db1"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app)
+
+// const vansCollectionRef = collection(db, "vans")
+
+// export async function getVans() {
+//   const querySnapshot = await getDocs(vansCollectionRef)
+//   const dataArr = querySnapshot.docs.map(doc => ({
+//       ...doc.data(),
+//       id: doc.id
+//   }))
+//   console.log(dataArr)
+//   return dataArr
+// }
+
+// export async function getVan(id) {
+//   const docRef = doc(db, "vans", id)
+//   const vanSnapshot = getDoc(docRef)
+//   return {
+//       ...vanSnapshot.data(),
+//       id: vanSnapshot.id
+//   }
+// }
+
+// export async function getHostVans() {
+//   const q = query(vansCollectionRef, where("hostId", "==", "123"))
+//   const querySnapshot = await getDocs(q)
+//   const dataArr = querySnapshot.docs.map(doc => ({
+//       ...doc.data(),
+//       id: doc.id
+//   }))
+//   return dataArr
+// }
+
 // Create a function to fetch the van data that has error handing added, so that in case of some sort of issues with the api, the page will have proper error handling and properly inform the user of the issues
 
 export async function getVans(id) {
